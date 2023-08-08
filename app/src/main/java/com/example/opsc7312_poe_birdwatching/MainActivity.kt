82883fa830327cd6948f3ac72d48897e72e85b79
Angelo Traverso <atraverso13@gmail.com>
2023-08-08 16:07:59 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/opsc7312_poe_birdwatching/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.opsc7312_poe_birdwatching
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
