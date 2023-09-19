9f347ca93b37f669135c1ae21e3eb25f109e4656
Angelo Traverso <atraverso13@gmail.com>
2023-09-19 16:00:19 +0200

    Menu button works - animation as well signin/signup UI complete

--- app/src/main/java/com/example/opsc7312_poe_birdwatching/SignIn.kt
+++ app/src/main/java/com/example/opsc7312_poe_birdwatching/SignIn.kt
@@ -1,10 +1,12 @@
 package com.example.opsc7312_poe_birdwatching
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +37,21 @@ class SignIn : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign_in, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Find the button by ID
+        val signInButton: Button = view.findViewById(R.id.btnSignIn)
+
+        // Set an OnClickListener to the button
+        signInButton.setOnClickListener {
+            // Handle button click here
+            // Start your activity when the button is clicked
+            val intent = Intent(activity, Hotpots::class.java)
+            startActivity(intent)
+        }
     }
 
     companion object {
