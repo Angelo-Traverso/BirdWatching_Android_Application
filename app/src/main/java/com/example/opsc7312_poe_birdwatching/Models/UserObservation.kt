c0a447003abacdd6c200f103c3520e9cb76c4ec0
Angelo Traverso <atraverso13@gmail.com>
2023-10-10 18:40:27 +0200

    Custom view for "MyObservations" added, dynamically adding based on list elements

--- app/src/main/java/com/example/opsc7312_poe_birdwatching/Models/UserObservation.kt
+++ app/src/main/java/com/example/opsc7312_poe_birdwatching/Models/UserObservation.kt
@@ -1,6 +1,7 @@
 package com.example.opsc7312_poe_birdwatching.Models
 
 import android.location.Geocoder
+import java.sql.Date
 import java.sql.Time
 
-data class UserObservation(val ObservationID: String, val UserID: String, val Date: Time, val BirdName: String, val Location: Geocoder)
+data class UserObservation(val ObservationID: String, val UserID: String, val Date: Date, val BirdName: String, val Location: Geocoder)
