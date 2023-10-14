0a1b3b42cb9e48a793ce3a502c38861c742df631
Angelo Traverso <atraverso13@gmail.com>
2023-10-14 15:14:38 +0200

    Commented BottomSheetHotspot.kt

--- app/src/main/java/com/example/opsc7312_poe_birdwatching/BottomSheetHotspot.kt
+++ app/src/main/java/com/example/opsc7312_poe_birdwatching/BottomSheetHotspot.kt
@@ -1,7 +1,6 @@
 package com.example.opsc7312_poe_birdwatching
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
@@ -17,37 +16,39 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class BottomSheetHotspot : BottomSheetDialogFragment() {
 
+    /*
+    * Button click listener for sheet button
+    * */
     private var buttonClickListener: (() -> Unit)? = null
 
+    /*
+    * Behaviour initialization for sheet
+    * */
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<View>
 
     /*
     * View Created
     * */
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.bottom_sheet_hotspot, container, false)
 
-        /*view.setBackgroundColor(Color.TRANSPARENT)*/
         // Find the button and set a click listener
         val startNavigationButton = view.findViewById<Button>(R.id.btnStartNavigation)
+
         startNavigationButton.setOnClickListener {
             // Notify the listener when the button is clicked
             buttonClickListener?.invoke()
         }
 
-
         return view
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        // Testing execution order for view created vs displaySightingsInBottomSheet
         Log.d("View!!!!", "View Created")
-        //bottomSheetView = view.findViewById(R.id.linearHotspotInformation)
 
         // Obtain the BottomSheetBehavior
         bottomSheetBehavior = BottomSheetBehavior.from(view.parent as View)
@@ -64,51 +65,57 @@ class BottomSheetHotspot : BottomSheetDialogFragment() {
             }
         })
 
-        // Find the view in the top part of the bottom sheet you want to click to expand
+        // Find the view in the top part of the bottom sheet
         val topViewToClick = view.findViewById<View>(R.id.tvBottomSheetHeading)
 
         topViewToClick.setOnClickListener {
             // Expand the bottom sheet when the top view is clicked
             bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
         }
-        bottomSheetBehavior.peekHeight = 600  // Change this value as needed
+        // Sets peek height of sheet
+        bottomSheetBehavior.peekHeight = 600
 
-        Log.d("List Size!!!!",ToolBox.hotspotSightings.size.toString())
+        // Binding linearView
         val bottomSheetLayout = view.findViewById<LinearLayout>(R.id.linearViewHotspotInformation)
 
         // Display the sightings in the bottom sheet
         displaySightingsInBottomSheet(bottomSheetLayout, ToolBox.hotspotSightings)
-
     }
 
     companion object {
         const val TAG = "BottomSheetHotspot"
     }
 
+    /*
+    * Button click listener for button on sheet
+    * */
     fun setButtonClickListener(listener: () -> Unit) {
         this.buttonClickListener = listener
     }
 
+    /*
+    * Dynamically displays hotspot data
+    * */
     @SuppressLint("InflateParams")
     fun displaySightingsInBottomSheet(bottomSheetView: LinearLayout, sightings: List<SightingModel>) {
+        // Testing execution order for view created vs displaySightingsInBottomSheet
         Log.d("Display!!!!", "Display called")
         val inflater = LayoutInflater.from(bottomSheetView.context)
 
         for (sighting in sightings) {
+
             // Inflate the hotspot_sighting layout
             val hotspotSightingView = inflater.inflate(R.layout.hotspot_sighting, null)
 
+            // TextViews
             val commonNameTextView = hotspotSightingView.findViewById<TextView>(R.id.tvCommonName)
             val howManyTextView = hotspotSightingView.findViewById<TextView>(R.id.tvHowMany)
             val dateTextView = hotspotSightingView.findViewById<TextView>(R.id.tvDate)
 
             // Set the sighting information in the included layout
             commonNameTextView.text = "Common Name: ${sighting.commonName}"
-            Log.d("Common name", sighting.commonName)
             howManyTextView.text = "How Many: ${sighting.howMany}"
-            Log.d("How Many?", sighting.howMany.toString())
             dateTextView.text = "Date: ${sighting.date}"
-            Log.d("Date", sighting.date)
 
             // Add the included layout to the bottom sheet
             bottomSheetView.addView(hotspotSightingView)
@@ -119,9 +126,9 @@ class BottomSheetHotspot : BottomSheetDialogFragment() {
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 2
             )
+
             line.setBackgroundColor(Color.BLACK)
             bottomSheetView.addView(line)
         }
     }
-
 }
