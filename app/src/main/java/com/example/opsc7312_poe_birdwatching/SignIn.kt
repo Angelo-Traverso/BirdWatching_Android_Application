b3d9c931c4483701e15cedaf1ba68b1950a20693
JPolakow <Jonathan@Polakow.co.za>
2023-10-19 12:51:58 +0200

    removed default login

--- app/src/main/java/com/example/opsc7312_poe_birdwatching/SignIn.kt
+++ app/src/main/java/com/example/opsc7312_poe_birdwatching/SignIn.kt
@@ -55,17 +55,8 @@ class SignIn : Fragment() {
             // Setting progress bar to visible when user attempts to sign in
             pbWaitToSignIn.visibility = View.VISIBLE
 
-            if (email == "atraverso@example.com" && pword == "Password123!") {
-                val newUser =
-                    UsersModel(ToolBox.users.count(), "Angelo", "Traverso" ,"atraverso@example.com", "", true, 5.0, 0)
-                ToolBox.users.add(newUser)
-                ToolBox.userID = ToolBox.users.indexOfFirst { it.Email == email }
-                println(ToolBox.userID)
-                val intent = Intent(activity, Hotpots::class.java)
-                startActivity(intent)
-            } else {
-                authenticateUser(email, pword)
-            }
+            authenticateUser(email, pword)
+
         }
     }
 
