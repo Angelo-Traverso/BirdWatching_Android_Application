95029549dc4b8b4560b15c93d729430d5a22766d
Angelo Traverso <atraverso13@gmail.com>
2023-11-12 12:49:04 +0200

    Cleaned up code and Added/Removed comments

--- app/src/main/java/com/example/opsc7312_poe_birdwatching/Models/SightingModel.kt
+++ app/src/main/java/com/example/opsc7312_poe_birdwatching/Models/SightingModel.kt
@@ -1,6 +1,6 @@
 package com.example.opsc7312_poe_birdwatching.Models
 
-//used to store data retrieved from eBird
+// Used to store data retrieved from eBird
 class SightingModel(
     val commonName: String,
     val howMany: Int,
